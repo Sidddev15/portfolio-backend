94c0f42f4b25da0d6bf9ef9b3614c16a8b4ef7bc
sidddev15 <siddharthpersonal1506@gmail.com>
2025-08-31 19:11:52 +0530

    feat(api): mount contact routes under /api/contact

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import projectsRouter from "./projects.routes.js";
 import experienceRouter from "./experience.routes.js";
 import educationRouter from "./education.routes.js";
 import techRouter from "./tech.routes.js";
+import contactRouter from "./contact.routes.js";
 
 const router = Router();
 
@@ -11,5 +12,6 @@ router.use("/projects", projectsRouter);
 router.use("/experience", experienceRouter);
 router.use("/education", educationRouter);
 router.use("/techstack", techRouter);
+router.use("/contact", contactRouter);
 
 export default router;
