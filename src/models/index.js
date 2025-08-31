2be41b7b1cc3bb5fdadbd2b0aaf06c85b811e84e
sidddev15 <siddharthpersonal1506@gmail.com>
2025-08-31 14:52:31 +0530

    chore(models): add index barrel export for cleaner imports

---
+++ src/models/index.js
@@ -0,0 +1,6 @@
+export { default as Project } from "./Project.js";
+export { default as Experience } from "./Experience.js";
+export { default as Education } from "./Education.js";
+export { default as Tech } from "./Tech.js";
+export { default as Message } from "./Message.js";
+export { default as Admin } from "./Admin.js";
